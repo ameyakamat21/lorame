cc0f427f645d13e3f0e19f76ab2b8d6a394f8d07
Ameya Kamat <ameyakamat21@gmail.com>
2017-04-03 22:33:44 -0400

    UART sending over FTDI implemented and working

--- Lorame-App/app/src/main/java/com/cmu/ece/build18/firstapp/MainActivity.java
+++ Lorame-App/app/src/main/java/com/cmu/ece/build18/firstapp/MainActivity.java
@@ -1,15 +1,23 @@
 package com.cmu.ece.build18.firstapp;
 
+import android.Manifest;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Context;
 import android.widget.Toast;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 
 import com.google.android.gms.analytics.Logger;
 
 //google play services imports
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
@@ -39,6 +47,8 @@ import com.hoho.android.usbserial.driver.UsbSerialPort;
 
 
 import android.hardware.usb.UsbDeviceConnection;
+
+import java.nio.charset.Charset;
 import java.util.List;
 import java.io.IOException;
 
@@ -50,21 +60,25 @@ public class MainActivity extends AppCompatActivity implements
     public static final String STATE_LONGITUDE = "STATE_LON";
     private GoogleApiClient mGoogleApiClient;
     private double currLatitude, currLongitude;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
 
         //initialize google play services interface
 
+        // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
-                .build();
+                .addApi(AppIndex.API).build();
 
-        if(savedInstanceState != null) {
+        if (savedInstanceState != null) {
             currLatitude = savedInstanceState.getDouble(STATE_LATITUDE);
             currLongitude = savedInstanceState.getDouble(STATE_LONGITUDE);
         }
@@ -74,26 +88,23 @@ public class MainActivity extends AppCompatActivity implements
     protected void onStart() {
         mGoogleApiClient.connect();
         super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.start(mGoogleApiClient, getIndexApiAction());
     }
 
     @Override
     protected void onStop() {
         mGoogleApiClient.disconnect();
-        super.onStop();
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(mGoogleApiClient, getIndexApiAction());
     }
 
     @Override
     public void onConnected(Bundle connectionHint) {
-        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
 
-        if (mLastLocation != null) {
-            currLatitude = mLastLocation.getLatitude();
-            currLongitude = mLastLocation.getLongitude();
-            showToast("Last loc: (" + currLatitude + ", " + currLongitude + ")");
-        } else  {
-            showToast("Last location was null.");
-        }
+        getAndDisplayLocation();
     }
 
     @Override
@@ -115,7 +126,48 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     protected void onDestroy() {
+
         super.onDestroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        for(int i=0; i<grantResults.length; i++) {
+            if(grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                showToast("Did not get permission for " + permissions[i] + ":(");
+                return;
+            }
+        }
+
+        showToast("Got all permissions!");
+    }
+
+    private void getAndDisplayLocation() {
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            Log.e("PERMISSION", "Does not have location permission!");
+            showToast("Does not have location permission!");
+
+
+            String[] permisionsToRequest = {Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION};
+            requestPermissions(permisionsToRequest, 1);
+        }
+
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            currLatitude = mLastLocation.getLatitude();
+            currLongitude = mLastLocation.getLongitude();
+            showToast("Last loc: (" + currLatitude + ", " + currLongitude + ")");
+        } else  {
+            showToast("Last location was null.");
+        }
     }
 
     private void showToast(String msg) {
@@ -125,31 +177,54 @@ public class MainActivity extends AppCompatActivity implements
         Toast toast = Toast.makeText(context, msg, duration);
         toast.show();
     }
+    
 
     public void sendUartMessage(View view) {
 
         UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
-        if (availableDrivers.isEmpty()) {
+
+        UsbSerialProber dflProber = UsbSerialProber.getDefaultProber();
+
+        List<UsbSerialDriver> availableDrivers = dflProber.findAllDrivers(manager);
+        if(availableDrivers == null) {
+            Log.e("DRIVER", "Available drivers list is null.");
+            showToast("Available drivers list is null.");
+        } else if (availableDrivers.isEmpty()) {
+            Log.e("DRIVER", "Available drivers list is empty...");
+            showToast("Available driver list is empty..");
             return;
+        }
+
+        for(UsbSerialDriver availableDriver : availableDrivers) {
+            showToast(availableDriver.toString());
+            Log.e("DRIVER", "Found driver: " + availableDriver);
         }
 
 // Open a connection to the first available driver.
         UsbSerialDriver driver = availableDrivers.get(0);
         UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+
         if (connection == null) {
             // You probably need to call UsbManager.requestPermission(driver.getDevice(), ..)
+            showToast("UsbDeviceConnection was null.");
             return;
         }
 
-// Read some data! Most have just one port (port 0).
+        // Read some data! Most have just one port (port 0).
         UsbSerialPort port = driver.getPorts().get(0);
         try {
             port.open(connection);
             port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
 
+            byte []sendBuf = "Hello".getBytes(Charset.forName("UTF-8"));
+            port.write(sendBuf, 1000);
+            textView.append("\n Sent buf " + "Hello");
+
+
             byte buffer[] = new byte[16];
             int numBytesRead = port.read(buffer, 1000);
+
+            textView.append("\n Recv buf: " + new String(buffer, "UTF-8"));
             Log.d("LOG", "Read " + numBytesRead + " bytes.");
         } catch (IOException e) {
             // Deal with error.
@@ -164,20 +239,22 @@ public class MainActivity extends AppCompatActivity implements
 
     public void getLocationBtnCallback(View view) {
 
-        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-
-        Log.e("LOG", "mGoogleApiClient.isConnected(): " + mGoogleApiClient.isConnected());
-
-        if (mLastLocation != null) {
-            currLatitude = mLastLocation.getLatitude();
-            currLongitude = mLastLocation.getLongitude();
-            showToast("Last loc: (" + currLatitude + ", " + currLongitude + ")");
-        } else  {
-            showToast("Last location was null.");
-            Log.e("LOCERR", "!!!!!!!! Last location was null.");
-        }
-        //showToast("(" + currLatitude + ", " + currLongitude + ")");
+       getAndDisplayLocation();
     }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
 }
