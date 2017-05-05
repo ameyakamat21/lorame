91d254f754cb7ce65963fca60b570a12fa14421d
Craig Hesling <craig@hesling.com>
2017-05-05 05:38:47 -0400

    Commented out Main

--- Android-FTDI-UART-Sample-master/app/src/main/java/com/ksksue/app/ftdi_uart/SerialNodeID.java
+++ Android-FTDI-UART-Sample-master/app/src/main/java/com/ksksue/app/ftdi_uart/SerialNodeID.java
@@ -10,25 +10,25 @@ public class SerialNodeID {
 		return String.format("%08X", id);
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ForwardMessage fmsg = new ForwardMessage();
-		fmsg.ids = new LinkedList<String>();
-		fmsg.ids.add("00112233");
-		fmsg.message = "Hello World!";
-		SerialPayload sp = new SerialPayload();
-		sp.putForwardMessage(fmsg);
-		System.out.println("Hello World!\n");
-		System.out.println("Payload: \"" + sp.toString() + "\"");
-		
-
-		System.out.printf("%02X\n", (byte)Integer.parseUnsignedInt("9A", 16));
-		ForwardMessage fmsg2 = new SerialPayload(sp.toString()).getForwardMessage();
-		System.out.println(fmsg2);
-		System.out.println("Message = \"" + fmsg2.message + "\"");
-		for(String id : fmsg2.ids) {
-			System.out.println(id);
-		}
-	}
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		ForwardMessage fmsg = new ForwardMessage();
+//		fmsg.ids = new LinkedList<String>();
+//		fmsg.ids.add("00112233");
+//		fmsg.message = "Hello World!";
+//		SerialPayload sp = new SerialPayload();
+//		sp.putForwardMessage(fmsg);
+//		System.out.println("Hello World!\n");
+//		System.out.println("Payload: \"" + sp.toString() + "\"");
+//		
+//
+//		System.out.printf("%02X\n", (byte)Integer.parseUnsignedInt("9A", 16));
+//		ForwardMessage fmsg2 = new SerialPayload(sp.toString()).getForwardMessage();
+//		System.out.println(fmsg2);
+//		System.out.println("Message = \"" + fmsg2.message + "\"");
+//		for(String id : fmsg2.ids) {
+//			System.out.println(id);
+//		}
+//	}
 
 }
